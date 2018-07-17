e5df9da555c1fcf9ca62b3a3ece6ba023cd8f482
Sean Christopher Febo <scf@techie.com>
2018-07-17 00:08:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ function driverNamesWithRevenueOver(array, revenue) {
   const newArray = driversWithRevenueOver(array, revenue);
   return newArray.map(name => name.name);
 }
+
+function exactMatch(array, object) { 
+  return array.filter(name => )
