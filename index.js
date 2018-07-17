ba78da2277367a0bbf16cff40330e202b91ed3e2
Sean Christopher Febo <scf@techie.com>
2018-07-17 00:01:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ function driversWithRevenueOver(array, revenue) {
   return array.filter(name => name.revenue > revenue); 
 }
   
+function driverNamesWithRevenueOver(array, revue) { 
+  return array.filter(name.name => name.revue > revenue);
+}
